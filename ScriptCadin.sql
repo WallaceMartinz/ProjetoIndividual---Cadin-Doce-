5705bad595fc7d0030a1c516cf8f6fa20ee0f95d
WallaceMartinz <wallace.mota@sptech.school>
2022-11-24 19:26:11 -0300

    backup

---
+++ ScriptCadin.sql
@@ -0,0 +1,17 @@
+create database CadinDoce;
+
+use CadinDoce;
+
+create table usuarios 
+	(idUsuarios int primary key auto_increment,
+	Nome varchar(65),
+    email varchar(65),
+    tel char(13), 
+    senha varchar(50));
+    
+    select*from usuarios;
+    
+    
+    
+    
+  
